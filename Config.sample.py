74164a4dd781ed3cf8204793c117e21cec6bf48f
ynk9h2h4r5-beep <ynk9h2h4r5@privaterelay.appleid.com>
2026-05-26 14:57:17 +0200

    Update Config.sample.py

--- Config.sample.py
+++ Config.sample.py
@@ -8,7 +8,7 @@ smtp_toaddrs = ["User <example@example.com>"]
 slack_webhook_url = ""
 
 # Telegram Token and ChatID for notification
-telegram_bot_token = ""
+telegram_bot_token = ${{ secrets.telegram_bot_token }}
 telegram_chat_id = ""
 
 # Vinted URL: change the TLD according to your country (.fr, .es, etc.)
